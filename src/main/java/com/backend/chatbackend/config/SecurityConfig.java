83d610b2bff7cfbbf68f36ee2789937cf44da356
lucasrluz <lucasr.luzbr@gmail.com>
2023-08-25 16:02:44 -0300

    feat: adicionado configuração do Spring Security

---
+++ src/main/java/com/backend/chatbackend/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.backend.chatbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+     private SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        httpSecurity.authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+            .anyRequest().authenticated()
+        );
+        
+        httpSecurity.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+}
