cd950cea82c3a45c25dd9bfaf4ab8a35b3e119df
lucasrluz <lucasr.luzbr@gmail.com>
2023-08-25 15:59:31 -0300

    feat: criado UserModel e UserRepository

---
+++ src/main/java/com/backend/chatbackend/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.backend.chatbackend.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.backend.chatbackend.models.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByEmail(String email);
+}
